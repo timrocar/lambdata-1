e1e5e4516b783b1268c74900f02d98fa1f395ad4
Tristan-Brown1096 <tristan.brown1096@gmail.com>
2020-08-31 16:18:04 -0500

    Added additional functionality and updated version number

--- lambdata/functions.py
+++ lambdata/functions.py
@@ -11,3 +11,17 @@ def date_split(df, col):
     df[col+'_month'] = list(df[col].dt.month)
     df[col+'_day'] = list(df[col].dt.day)
     df[col+'_year'] = list(df[col].dt.year)
+
+def wrangle(X, hc=100):
+  #copy X
+  X = X.copy()
+
+  #drop constant columns
+  X = X.drop([col for col in X if X[col].nunique()==1], axis=1)
+
+  #drop high cardinality columns
+  hc = [col for col in X.describe(include='object').columns if X[col].nunique() > card]
+  X = X.drop(hc, axis=1)
+
+  #return new version 
+  return X
