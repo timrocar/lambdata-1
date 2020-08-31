19109b1003de687397a095e4e09592dfb5710a9c
Tristan-Brown1096 <tristan.brown1096@gmail.com>
2020-08-31 12:46:55 -0500

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+""" lambdata - a collection of Data Science helper functions
+"""
+import setuptools
+
+REQUIRED = [
+    "numpy",
+    "pandas"
+]
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.head()
+
+setuptools.setup(
+    name="lambdata-Tristan-Brown1096",
+    version="0.0.1",
+    author="Tristan-Brown1096",
+    description="A collection of Data Science helper functions",
+    long_description=LONG_DESCRIPTION,
+    long_description_content="text/markdown",
+    url="https://github.com/Tristan-Brown1096/lambdata",
+    package=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=REQUIRED,
+    classifiers=[
+        "Programming Language :: python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
