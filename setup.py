b1e1fc7a8ea226df14d844ffe48ba28b094c10f3
Tristan-Brown1096 <tristan.brown1096@gmail.com>
2020-09-01 16:40:46 -0500

    corrected typo in setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     long_description=LONG_DESCRIPTION,
     long_description_content="text/markdown",
     url="https://github.com/Tristan-Brown1096/lambdata",
-    package=setuptools.find_packages(),
+    packages=setuptools.find_packages(),
     python_requires=">=3.6",
     install_requires=REQUIRED,
     classifiers=[
