15289693e2418dc370c3fdb6e9733d54f6bbf904
Tristan-Brown1096 <tristan.brown1096@gmail.com>
2020-09-01 15:01:04 -0500

    updated version number and added a dataframe class with previous functions as methods

--- lambdata/functions.py
+++ lambdata/functions.py
@@ -1,27 +1,44 @@
+'''The functions of the lambdata package
+'''
+# Prints the contingency table and chi2 stats
+# given two columns of a DataFrame
+
+
 def cont_chi2(col1, col2):
-  contingency = pd.crosstab(col1, col2)
-  print(contingency)
-  chi2, p_value, dof, expected = stats.chi2_contingency(contingency)
-  print("chi2 statistic", chi2)
-  print("p value", p_value)
-  print("degrees of freedom",dof)
-  print("expected frequencies table \n", expected)
+    contingency = pd.crosstab(col1, col2)
+    print(contingency)
+    chi2, p_value, dof, expected = stats.chi2_contingency(contingency)
+    print("chi2 statistic", chi2)
+    print("p value", p_value)
+    print("degrees of freedom", dof)
+    print("expected frequencies table \n", expected)
 
-def date_split(df, col):
-    df[col+'_month'] = list(df[col].dt.month)
-    df[col+'_day'] = list(df[col].dt.day)
-    df[col+'_year'] = list(df[col].dt.year)
 
-def wrangle(X, hc=100):
-  #copy X
-  X = X.copy()
+# Splits dates into new columns in a DataFrame
+def date_split(df):
+    [col for col in X if X[col].dtype == 'DateTime64' 
+        df[col+'_month'] = list(df[col].dt.month)
+        df[col+'_day'] = list(df[col].dt.day)
+        df[col+'_year'] = list(df[col].dt.year)]
 
-  #drop constant columns
-  X = X.drop([col for col in X if X[col].nunique()==1], axis=1)
 
-  #drop high cardinality columns
-  hc = [col for col in X.describe(include='object').columns if X[col].nunique() > card]
-  X = X.drop(hc, axis=1)
+def wrangle(X, cardinality=100):
+    # copy X
+    X = X.copy()
 
-  #return new version 
-  return X
+    # drop constant columns
+    X = X.drop([col for col in X if X[col].nunique() == 1], axis=1)
+
+    # drop high cardinality columns
+    hc = [col for col in X.describe(include='object').columns
+          if X[col].nunique() > cardinality]
+    X = X.drop(hc, axis=1)
+
+    # return wrangled DataFrame
+    return X
+
+
+class MyDataFrame(pd.DataFrame):
+    wrangle(X=self)
+    cont_chi2()
+    date_split(df=self)
